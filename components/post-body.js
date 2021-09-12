d0a0d53c21c88c56844f77b1ec7dabf8fc8d903d
mich45 <hungbomichael@gmail.com>
2021-09-12 14:29:33 +0100

    first commit

--- components/post-body.js
+++ components/post-body.js
@@ -1,12 +1,36 @@
-import markdownStyles from './markdown-styles.module.css'
+import { useRef, useEffect } from 'react';
+import markdownStyles from './markdown-styles.module.css';
+import hljs from 'highlight.js/lib/core';
+import javascript from 'highlight.js/lib/languages/javascript';
+hljs.registerLanguage('javascript', javascript);
+import Head from 'next/head';
 
 export default function PostBody({ content }) {
+  const node = useRef();
+  useEffect(() => {
+    if (node) {
+      const nodes = node.current.querySelector('pre');
+      hljs.highlightAll(nodes);
+      console.log(nodes);
+    }
+  });
+
   return (
-    <div className="max-w-2xl mx-auto">
-      <div
-        className={markdownStyles['markdown']}
-        dangerouslySetInnerHTML={{ __html: content }}
-      />
-    </div>
-  )
+    <>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.2.0/build/styles/github-dark-dimmed.min.css"
+        />
+        <script src="https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.2.0/build/languages/javascript.min.js"></script>
+      </Head>
+      <div className="max-w-2xl mx-auto ">
+        <div
+          ref={node}
+          className={markdownStyles['markdown']}
+          dangerouslySetInnerHTML={{ __html: content }}
+        />
+      </div>
+    </>
+  );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
-import '../styles/index.css'
+import '../styles/index.css';
+import Head from 'next/head';
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  );
 }
